9cf9ff58cb77b52b933f78019e284545748b5738
codedbylukas <lukaswittek@gmail.com>
2026-04-26 13:01:26 -0500

    test imports

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -11,6 +11,10 @@ def test_callable_encrypt_file():
 def test_callable_decrypt_file():
     assert callable(decrypt_file)
 
+def test_import():
+    from src.crypto import fernet, exit
+    assert callable(fernet)
+    assert callable(exit)
 
 def test_encypt_file_with_wrong_key():
     with pytest.raises(SystemExit) as excinfo:
