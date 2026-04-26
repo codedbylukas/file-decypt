2900c184159e26202a3c3c59202b552c81682c0a
codedbylukas <lukaswittek@gmail.com>
2026-04-26 13:19:27 -0500

    add

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -24,6 +24,12 @@ def test_decrypt_file_with_wrong_key():
     assert excinfo.value.code == 1
 
 
+def test_encrypt_fail():
+    with pytest.raises(SystemExit) as excinfo:
+        encrypt_file("test_files/fail.txt", "wrong_key")
+    assert excinfo.value.code == 1
+
+
 def test_test_files_dir_exists():
     assert Path("test_files").exists()
     assert Path("test_files").is_dir()
@@ -53,9 +59,29 @@ def test_encypt_file():
         pytest.fail(f"An error occurred: {e}")
 
 
-def test_encrypt_and_decrypt_file():
+def test_decrypt_file():
     try:
         test_file = Path("test_files/test1.txt")
+        with open(test_file, "w") as file:
+            file.write("Hello World")
+        key = fernet.Fernet.generate_key()
+        encrypt_file(str(test_file), key)
+        with open(test_file, "r") as file:
+            encrypted_data = file.read()
+        assert encrypted_data != "Hello World"
+        decrypt_file(str(test_file), key)
+        with open(test_file, "r") as file:
+            decrypted_data = file.read()
+        assert decrypted_data == "Hello World"
+        with open(test_file, "w") as file:
+            file.write("Hello World")
+    except Exception as e:
+        pytest.fail(f"An error occurred: {e}")
+
+
+def test_encrypt_and_decrypt_file():
+    try:
+        test_file = Path("test_files/test2.txt")
         with open(test_file, "w") as file:
             file.write("Hello World")
         key = fernet.Fernet.generate_key()
