67b162ae53c7dc71230c75abef013ae001fc806b
codedbylukas <lukaswittek@gmail.com>
2026-04-26 10:39:23 -0500

    add more keybord interopt handeling crypto

--- crypto.py
+++ crypto.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 from cryptography import fernet
 
 
@@ -10,10 +12,12 @@ def encrypt_file(file_path, key):
         encrypted_data = fernet_instance.encrypt(encrypted_data)
         with open(file_path, "wb") as file:
             file.write(encrypted_data)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit(0)
     except Exception as e:
         print(f"An error occurred while encrypting:  {file_path}: {e}")
-        input("Press Enter to exit...")
-        exit()
+        exit(1)
 
 
 def decrypt_file(file_path, key):
@@ -26,7 +30,9 @@ def decrypt_file(file_path, key):
 
         with open(file_path, "wb") as file:
             file.write(decrypted_data)
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit(0)
     except Exception as e:
         print(f"An error occurred during decryption: {file_path}: {e}")
-        input("Press Enter to exit...")
-        exit()
+        exit(1)
