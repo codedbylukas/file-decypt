b8e1e6281edfbd93d65b3bedd2d5749b1096aa30
codedbylukas <lukaswittek@gmail.com>
2026-04-26 10:59:35 -0500

    fixed main

--- main.py
+++ main.py
@@ -1,50 +1,12 @@
 from sys import exit
 
 from cryptography import fernet
-from src.crypto import encrypt_file, decrypt_file
-from src.get_all_files import file_paths, get_all_files_fnc
 from src.starting import folder_or_file, encrypting_or_decrypting, check_key
+from src.main_helper import if_file, store_key_encrypted, get_key_encrypted, when_its_an_folder
 
 key = None  # Initialize key globally
 input_choice: str = "None"
 int_choice: int = None
-
-
-def if_file(encrypting: bool, decrypting: bool, file_path: str, key: bytes):
-    if encrypting:
-        encrypt_file(file_path, key)
-    elif decrypting:
-        decrypt_file(file_path, key)
-
-
-def store_key_encrypted():
-    print("You chose to encrypt the files.")
-    print("Schlüssel: ", key.decode())
-    with open("key_file.txt", "w") as key_file:
-        key_file.write(key.decode())
-    print("Please keep the key safe, otherwise this can never be decrypted again.")
-
-
-def get_key_encrypted():
-    print("You chose to decrypt the files.")
-    with open("key_file.txt", "r") as key_file:
-        key_input = key_file.read()
-        key = key_input.encode()
-
-
-def when_its_an_folder(
-    folder_path: str, key: bytes, encrypting: bool, decrypting: bool
-):
-    if not folder_path:
-        print("Error: Folder path is not set. Exiting.")
-        exit(1)
-    get_all_files_fnc(folder_path)
-    for file in file_paths:
-        if encrypting:
-            encrypt_file(file, key)
-        elif decrypting:
-            decrypt_file(file, key)
-
 
 def main():
     global key
@@ -52,7 +14,7 @@ def main():
     encrypting, decrypting, key = encrypting_or_decrypting(None, None, key)
     check_key(key)
     if encrypting:
-        store_key_encrypted()
+        store_key_encrypted(key)
     elif decrypting:
         get_key_encrypted()
     if folder:
---
+++ src/main_helper.py
@@ -0,0 +1,38 @@
+from src.crypto import encrypt_file, decrypt_file
+from src.get_all_files import file_paths, get_all_files_fnc
+
+def if_file(encrypting: bool, decrypting: bool, file_path: str, key: bytes):
+    if encrypting:
+        encrypt_file(file_path, key)
+    elif decrypting:
+        decrypt_file(file_path, key)
+
+
+def store_key_encrypted(key: bytes):
+    print("You chose to encrypt the files.")
+    print("Schlüssel: ", key.decode())
+    with open("key_file.txt", "w") as key_file:
+        key_file.write(key.decode())
+    print("Please keep the key safe, otherwise this can never be decrypted again.")
+
+
+def get_key_encrypted():
+    print("You chose to decrypt the files.")
+    with open("key_file.txt", "r") as key_file:
+        key_input = key_file.read()
+        key = key_input.encode()
+
+
+def when_its_an_folder(
+    folder_path: str, key: bytes, encrypting: bool, decrypting: bool
+):
+    if not folder_path:
+        print("Error: Folder path is not set. Exiting.")
+        exit(1)
+    get_all_files_fnc(folder_path)
+    for file in file_paths:
+        if encrypting:
+            encrypt_file(file, key)
+        elif decrypting:
+            decrypt_file(file, key)
+
