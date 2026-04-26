f4f915aa14349428f42de070d3c8801b7e315698
codedbylukas <lukaswittek@gmail.com>
2026-04-26 10:19:00 -0500

    add extra functionality

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+from sys import exit
+
 from cryptography import fernet
 from crypto import encrypt_file, decrypt_file
 from get_all_files import file_paths, get_all_files_fnc
 from starting import folder_or_file, encrypting_or_decrypting, check_key
 
 key = None  # Initialize key globally
-input_choice = "None"
-int_choice = None
-key = None
+input_choice: str = "None"
+int_choice: int = None
+
+
+def if_file(encrypting: bool, decrypting: bool, file_path: str, key: bytes):
+    if encrypting:
+        encrypt_file(file_path, key)
+    elif decrypting:
+        decrypt_file(file_path, key)
 
 
 def store_key_encrypted():
@@ -24,6 +32,20 @@ def get_key_encrypted():
         key = key_input.encode()
 
 
+def when_its_an_folder(
+    folder_path: str, key: bytes, encrypting: bool, decrypting: bool
+):
+    if not folder_path:
+        print("Error: Folder path is not set. Exiting.")
+        exit(1)
+    get_all_files_fnc(folder_path)
+    for file in file_paths:
+        if encrypting:
+            encrypt_file(file, key)
+        elif decrypting:
+            decrypt_file(file, key)
+
+
 def main():
     global key
     folder, file, folder_path, file_path = folder_or_file(None, None)
@@ -34,20 +56,9 @@ def main():
     elif decrypting:
         get_key_encrypted()
     if folder:
-        if not folder_path:
-            print("Error: Folder path is not set. Exiting.")
-            exit()
-        get_all_files_fnc(folder_path)
-        for file in file_paths:
-            if encrypting:
-                encrypt_file(file, key)
-            elif decrypting:
-                decrypt_file(file, key)
+        when_its_an_folder(folder_path, key, encrypting, decrypting)
     elif file:
-        if encrypting:
-            encrypt_file(file_path, key)
-        elif decrypting:
-            decrypt_file(file_path, key)
+        if_file(encrypting, decrypting, file_path, key)
     print("Process completed.")
 
 
@@ -56,5 +67,5 @@ if __name__ == "__main__":
         main()
         do_exit = input("Do you want to exit? (y/n): ")
         if do_exit.lower() == "y":
-            break
+            exit(0)
     input("Press Enter to exit...")
